d1acf9ff37899e20ca57cdd8c5501ddeb726598c
athlaliel <athla8452@gmail.com>
2020-11-17 22:45:45 +0900

    #34 バリデーションの記述

--- app/models/send_history.rb
+++ app/models/send_history.rb
@@ -1,2 +1,8 @@
 class SendHistory < ApplicationRecord
+  # 全てのカラムにおいて値が存在する
+  validates: :amount, :from_user, :to_user, presense: true
+  # from_user_idとto_user_idは異なる値であること
+  validates: :from_user, :to_user, uniqueness: true
+  # amountは0以上
+  validates: :amount, {numericality: true, greater_than_or_equal_to: {minimum: 0}}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  #name、account_nameは空投稿は不可、文字数制限が50文字まで
+  validates :name, :account_name, {presence: true, length: {maximum: 50}}
+  #account_nameは一意性なので重複する名前は登録不可
+  validates :account_name, uniqueness: true
+  #emailは空入力不可、文字数は256文字まで、一意性なので重複する名前は登録不可。行を分ける事でエラー文の重複回避
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, length: {maximum: 256}, uniqueness: true
 end
