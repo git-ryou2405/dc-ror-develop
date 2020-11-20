c2d5fc23cf4d250f014f1674c2626b9a74544864
athlaliel <athla8452@gmail.com>
2020-11-20 14:24:04 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates :account_name, uniqueness: true
   #emailは空入力不可、文字数は256文字まで、一意性なので重複する名前は登録不可。行を分ける事でエラー文の重複回避
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: {with: VALID_EMAIL_REGEX}
-  validates :email, presence: true, length: {maximum: 256}, uniqueness: true
+  validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+  validates :email, presence: true, length: {maximum: 256}, uniqueness: true 
+  validates :is_admin, presence: true
 end
