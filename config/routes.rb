13c48369098310812291f5979edd657781914286
ryou <r2405.exe@gmail.com>
2020-11-23 18:56:19 +0900

    #35 fix from resource :user_balance to :journals

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'users#index'
   
   resources :users, except: :destroy
-  resources :user_balances, only: [:index, :new, :create]
+  resources :journals, only: [:index, :new, :create]
 end
