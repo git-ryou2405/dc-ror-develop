87ca0f72a5964c845b246e0e0283450c14d9e139
酒井彦行 <hikoyuki.sakai@gmail.com>
2020-11-25 00:58:40 +0900

    rubcop実行・修正

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,6 +1,6 @@
 class Journal < ApplicationRecord
-  belongs_to :to_user, class_name: 'User', foreign_key: 'to_user_id'
-  belongs_to :from_user, class_name: 'User', foreign_key: 'from_user_id'
+  belongs_to :to_user, class_name: "User", foreign_key: "to_user_id", inverse_of: "receipt_historie"
+  belongs_to :from_user, class_name: "User", foreign_key: "from_user_id", inverse_of: "send_histories"
 
   enum send_type: {
     fee: 1,  # 業務委託報酬
@@ -8,5 +8,4 @@ class Journal < ApplicationRecord
     issuance: 8, # コイン発行
     cashing: 9, # コイン回収
   }, _prefix: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :receipt_histories, class_name: 'Journal', foreign_key: 'to_user_id'
-  has_many :send_histories, class_name: 'Journal', foreign_key: 'from_user_id'
-  has_one :user_balance
+  has_many :receipt_histories, class_name: "Journal", foreign_key: "to_user_id",
+                               inverse_of: "to_user", dependent: :destroy
+  has_many :send_histories, class_name: "Journal", foreign_key: "from_user_id",
+                            inverse_of: "from_user", dependent: :destroy
+  has_one :user_balance, dependent: :destroy
 end
