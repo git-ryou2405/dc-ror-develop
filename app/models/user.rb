62d5b55dc58effcdf95166344e81bb835ad1cd53
athlaliel <athla8452@gmail.com>
2020-11-26 13:53:16 +0900

    #34 Eメールカラムのuniquenesの短縮修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :account_name, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
-  validates :email, presence: true, length: {maximum: 256}, uniqueness: true, confirmation: { case_sensitive: false }
+  validates :email, presence: true, length: {maximum: 256}, uniqueness: { case_sensitive: false }
   validates :is_admin, presence: true
 end
