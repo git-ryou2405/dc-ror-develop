ec9ea797b13301e7fcf48b16fcc13a2feff80201
ryou <r2405.exe@gmail.com>
2020-11-26 19:51:36 +0900

    #51 rails dvise:controllers users実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_060113) do
+ActiveRecord::Schema.define(version: 2020_11_26_100513) do
+
+  create_table "cotrollers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "users"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_cotrollers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_cotrollers_on_reset_password_token", unique: true
+  end
 
   create_table "journals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "from_user_id"
