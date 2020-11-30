7a5ec6ec9f08bc806ed0a941872601917d44c149
ryou <r2405.exe@gmail.com>
2020-11-30 23:30:30 +0900

    #51 controller.rbを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :receipt_histories, class_name: "Journal", foreign_key: "to_user_id",
