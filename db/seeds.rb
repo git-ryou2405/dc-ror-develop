f01f72f78ed1384110b1ce1ddae9c0d3ac92aedd
git-ryou2405 <r2405.exe@gmail.com>
2020-12-06 01:50:07 +0900

    seed再修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "user1", account_name: "@user1", email: "user1@qa.com", password: "111111")
-User.create!(name: "user2", account_name: "@user2", email: "user2@qa.com", password: "111111")
+# ユーザー2名分作成
+1.upto(2) do |i|
+  User.create!(name: "user#{i}", account_name: "user_#{i}", email: "user_#{i}@test.com", is_admin: false, password: "#{i}+#{i}+#{i}+#{i}+#{i}+#{i}")
+end
 
-UserBalance.create!(user_id: 1, last_calculated_journal_id: 1, send_total_amount: "0", receipt_total_amount: "10000")
-UserBalance.create!(user_id: 2, last_calculated_journal_id: 2, send_total_amount: "0", receipt_total_amount: "10000")
+# user1 が user2 に送金した履歴3件
+amount_from_user1 = 1000
+3.times do
+  Journal.create!(from_user_id: 1, to_user_id: 2, amount: amount_from_user1, send_type: "fee")
+  amount_from_user1 += 1000
+end
+
+# user2 が user1 に送金した履歴3件
+amount_from_user2 = 100
+3.times do
+  Journal.create!(from_user_id: 2, to_user_id: 1, amount: amount_from_user2, send_type: "fee")
+  amount_from_user2 += 100
+end
+
+# user1 user2 の残高状態
+UserBalance.create!(user_id: 1, last_calculated_journal_id: 3, send_total_amount: "3000", receipt_total_amount: "10000")
+UserBalance.create!(user_id: 2, last_calculated_journal_id: 6, send_total_amount: "300", receipt_total_amount: "10000")
