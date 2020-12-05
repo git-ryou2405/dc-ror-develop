87f4f0a1332babd633f7354d9a7cc9597dc2be61
git-ryou2405 <r2405.exe@gmail.com>
2020-12-05 20:35:31 +0900

    seed設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "user1", account_name: "@user1", email: "user1@qa.com", password: "111111")
+User.create!(name: "user2", account_name: "@user2", email: "user2@qa.com", password: "111111")
+
+UserBalance.create!(user_id: 1, last_calculated_journal_id: 0, send_total_amount: "0", receipt_total_amount: "10000")
+UserBalance.create!(user_id: 2, last_calculated_journal_id: 0, send_total_amount: "0", receipt_total_amount: "10000")
